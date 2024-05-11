81bde3a732ce019a9193184f3dae7727cb63d115
sandesb <bajracharyasandeshh@gmail.com>
2024-05-11 10:43:29 +0545

    date-fix

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import React, { useState } from "react";
-
+import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from 'react-datepicker';
 
 const Main =() =>{
