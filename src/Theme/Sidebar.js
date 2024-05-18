0b27df871210f2621fc8114e5206206b67bf04b4
Your Name <your@email.com>
2024-05-18 17:36:57 +0545

    active status

--- src/Theme/Sidebar.js
+++ src/Theme/Sidebar.js
@@ -1,21 +1,40 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
 import { FaHome, FaTasks, FaCalendarAlt, FaUser, FaInfoCircle } from 'react-icons/fa';
 
-const Sidebar =() =>{
-    
+const Sidebar = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
 
-  return(
-    <div class="sidebar">
-    <ul>
-        <li > <FaHome style={{ color: 'rgb(95, 95, 95)', fontSize: '22px' }} />   <Link to="">Dashboard</Link></li>
-        <li><FaTasks style={{ color: 'rgb(95, 95, 95)', fontSize: '22px'}}/> <Link to="Task">All Tasks</Link></li>
-        <li><FaCalendarAlt style={{ color: 'rgb(95, 95, 95)', fontSize: '22px'}}/> <Link to="Scheduled">Scheduled Tasks</Link></li>
-        <li> <FaUser style={{ color: 'rgb(95, 95, 95)', fontSize: '22px'}} /> <Link to="profile"> My profile</Link></li>
-        <li><FaInfoCircle style={{ color: 'rgb(95, 95, 95)', fontSize: '22px'}} /> <Link to="settings"> About Us</Link></li>
-    </ul>
-    
-</div>
-);
+  const handleClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="sidebar">
+      <ul>
+        <li className={activeIndex === 0 ? "active" : ""} onClick={() => handleClick(0)}>
+          <FaHome className={activeIndex === 0 ? "active-icon" : ""} style={{ fontSize: '22px' }} />
+          <Link to="/">Dashboard</Link>
+        </li>
+        <li className={activeIndex === 1 ? "active" : ""} onClick={() => handleClick(1)}>
+          <FaTasks className={activeIndex === 1 ? "active-icon" : ""} style={{ fontSize: '22px' }} />
+          <Link to="/Task">All Tasks</Link>
+        </li>
+        <li className={activeIndex === 2 ? "active" : ""} onClick={() => handleClick(2)}>
+          <FaCalendarAlt className={activeIndex === 2 ? "active-icon" : ""} style={{ fontSize: '22px' }} />
+          <Link to="/Scheduled">Scheduled Tasks</Link>
+        </li>
+        <li className={activeIndex === 3 ? "active" : ""} onClick={() => handleClick(3)}>
+          <FaUser className={activeIndex === 3 ? "active-icon" : ""} style={{ fontSize: '22px' }} />
+          <Link to="/profile">My Profile</Link>
+        </li>
+        <li className={activeIndex === 4 ? "active" : ""} onClick={() => handleClick(4)}>
+          <FaInfoCircle className={activeIndex === 4 ? "active-icon" : ""} style={{ fontSize: '22px' }} />
+          <Link to="/settings">About Us</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
+
 export default Sidebar;
