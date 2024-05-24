d2faffc2862bf33134f4a4ba6d7ac2fb88282459
Your Name <your@email.com>
2024-05-24 07:08:37 +0545

    changes

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -90,7 +90,7 @@ const Landing = ( ) => {
                     <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/>
                 </svg> 
                 <blockquote>
-                    <p class="text-xl font-medium text-gray-900 md:text-2xl dark:text-white">"Great for group projects: multiple reminders when something is due; clarity on what to do next; white noises for focus; and the ability to share lists."</p>
+                    <p class="text-xl font-medium text-gray-900 md:text-2xl dark:text-white">"My favorite app of the entire year which is my to-do app; clarity on what to do next; white noises for focus. It's called ListFul."</p>
                 </blockquote>
                 <figcaption class="flex items-center justify-center mt-6 space-x-3">
                     <img class="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="profile picture"/>
